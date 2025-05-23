0203ada88a8b783cd6c2ba670ce387a3bb99ecc6
sandro12352 <157259489+sandro12352@users.noreply.github.com>
2025-05-23 03:00:04 -0500

    haciendo responsivo

--- src/app/sdrop/pages/layout/layout.component.ts
+++ src/app/sdrop/pages/layout/layout.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+declare var bootstrap: any;
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
 
+
+export class LayoutComponent implements AfterViewInit{
+
+
+  ngAfterViewInit(): void {
+    const links = document.querySelectorAll('.ir-link1');
+    links.forEach(link => {
+      link.addEventListener('click',()=>{
+        const offcanvasElement = document.querySelector('.offcanvas');
+        const offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvasElement);
+        if (offcanvasInstance) {
+          offcanvasInstance.hide();
+        }
+      })
+    })
+  }
+  
+  
 
 }
