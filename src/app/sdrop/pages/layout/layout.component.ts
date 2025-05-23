b93f544d8915d7c8edd457a9b3d7bfef16cf8454
sandro12352 <157259489+sandro12352@users.noreply.github.com>
2025-05-23 12:42:06 -0500

    error del document corregido

--- src/app/sdrop/pages/layout/layout.component.ts
+++ src/app/sdrop/pages/layout/layout.component.ts
@@ -1,29 +1,29 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import { AfterViewInit, Component, PLATFORM_ID, Inject } from '@angular/core';
+
 declare var bootstrap: any;
+
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
+export class LayoutComponent implements AfterViewInit {
 
-
-export class LayoutComponent implements AfterViewInit{
-
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngAfterViewInit(): void {
-    const links = document.querySelectorAll('.ir-link1');
-    links.forEach(link => {
-      link.addEventListener('click',()=>{
-        const offcanvasElement = document.querySelector('.offcanvas');
-        const offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvasElement);
-        if (offcanvasInstance) {
-          offcanvasInstance.hide();
-        }
-      })
-    })
+    if (isPlatformBrowser(this.platformId)) {
+      const links = document.querySelectorAll('.ir-link1');
+      links.forEach(link => {
+        link.addEventListener('click', () => {
+          const offcanvasElement = document.querySelector('.offcanvas');
+          const offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvasElement);
+          if (offcanvasInstance) {
+            offcanvasInstance.hide();
+          }
+        });
+      });
+    }
   }
-  
-  
-
 }
